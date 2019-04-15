2c0d9b46a25cf2e946f8465c721c1e8835ea2f63
SeverinJB <32867365+SeverinJB@users.noreply.github.com>
2019-04-15 17:20:34 +0200

    Improve readability

--- js/script.js
+++ js/script.js
@@ -6,16 +6,16 @@ function randomTheme(){
   var listThemes = ["style1","style2","style3","style4","style5","style6",];
   var listDocs = ["docs/bloomberg/Bloomberg_final.html","docs/eudirective/EUDirective_final.html","docs/harpers/Harpers_Final.html","docs/huffington/Huffington_Final.html","docs/thecut/JessicaPresler.html","docs/tls/TimesLiterarySupplement_Final.html",];
 
-  var iframe = document.getElementById("document");
+  var frameContent = document.getElementById("frameDocument");
 
   var choiceTheme = listThemes[Math.floor(Math.random()*listThemes.length)];
   var choiceDoc = listDocs[Math.floor(Math.random()*listDocs.length)];
 
-  document.getElementById("document").setAttribute("src", choiceDoc);
+  document.getElementById("frameDocument").setAttribute("src", choiceDoc);
   document.getElementById("currentDoc").innerHTML = choiceDoc;
 
-  iframe.addEventListener("load", function() {
-    var elmnt = iframe.contentWindow.document.getElementsByName("theme")[0];
+  frameContent.addEventListener("load", function() {
+    var elmnt = frameContent.contentWindow.document.getElementsByName("theme")[0];
     elmnt.setAttribute("href", "../../css/" + choiceTheme + ".css");
     document.getElementById("currentTheme").innerHTML = choiceTheme;
   });
@@ -26,8 +26,8 @@ function randomTheme(){
 };
 
 function switchTheme(input){
-  var iframe = document.getElementById("document");
-  var elmnt = iframe.contentWindow.document.getElementsByName("theme")[0];
+  var frameContent = document.getElementById("frameDocument");
+  var elmnt = frameContent.contentWindow.document.getElementsByName("theme")[0];
 
   document.getElementById("currentTheme").innerHTML = input.name;
   elmnt.setAttribute("href", "../../css/" + input.name + ".css");
@@ -39,7 +39,7 @@ function switchTheme(input){
 
 function switchDoc(input){
   document.getElementById("currentDoc").innerHTML = input.name;
-  document.getElementById("document").setAttribute("src", "docs/" + input.name + ".html");
+  document.getElementById("frameDocument").setAttribute("src", "docs/" + input.name + ".html");
 
   if ($("#menu-nav-wrapper").hasClass("active")) {
     menuToggle();
@@ -48,7 +48,7 @@ function switchDoc(input){
 
 function switchPage(input){
   document.getElementById("currentPage").innerHTML = input.name;
-  document.getElementById("document").setAttribute("src", "pages/" + input.name + ".html");
+  document.getElementById("frameDocument").setAttribute("src", "pages/" + input.name + ".html");
 
   if ($("#menu-nav-wrapper").hasClass("active")) {
     menuToggle();
