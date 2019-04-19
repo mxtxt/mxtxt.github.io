a62d9892ea5ef8455509f9065d7b71d3fa1e95b6
SeverinJB <32867365+SeverinJB@users.noreply.github.com>
2019-04-19 11:59:22 +0200

    Update navHighlight.js

--- js/navHighlight.js
+++ js/navHighlight.js
@@ -1,9 +1,9 @@
 
 // switchDoc
-var nav = document.getElementById("doc-nav");
-var btns = nav.getElementsByClassName("doc-btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
+var doc_nav = document.getElementById("doc-nav");
+var doc_btns = nav.getElementsByClassName("doc-btn");
+for (var i = 0; i < doc_btns.length; i++) {
+  doc_btns[i].addEventListener("click", function() {
   var current = document.getElementsByClassName("active");
   if (current.length > 0) {
     current[0].className = current[0].className.replace(" active", "");
@@ -14,10 +14,10 @@ for (var i = 0; i < btns.length; i++) {
 
 
 // switchTheme
-var nav = document.getElementById("theme-nav");
-var btns = nav.getElementsByClassName("theme-btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
+var theme_nav = document.getElementById("theme-nav");
+var theme_btns = nav.getElementsByClassName("theme-btn");
+for (var i = 0; i < theme_btns.length; i++) {
+  theme_btns[i].addEventListener("click", function() {
   var current = document.getElementsByClassName("active");
   if (current.length > 0) {
     current[0].className = current[0].className.replace(" active", "");
@@ -28,10 +28,10 @@ for (var i = 0; i < btns.length; i++) {
 
 
 // switchPage
-var nav = document.getElementById("menu-nav");
-var btns = nav.getElementsByClassName("menu-btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
+var menu_nav = document.getElementById("menu-nav");
+var menu_btns = nav.getElementsByClassName("menu-btn");
+for (var i = 0; i < menu_btns.length; i++) {
+  menu_btns[i].addEventListener("click", function() {
   var current = document.getElementsByClassName("active");
   if (current.length > 0) {
     current[0].className = current[0].className.replace(" active", "");
