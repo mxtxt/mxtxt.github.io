899a95937f09a1f7b82957cfffc59f57cf911175
SeverinJB <32867365+SeverinJB@users.noreply.github.com>
2019-04-27 12:23:20 +0200

    Update circular-audio-wave.js

--- aurora/circular-audio-wave.js
+++ aurora/circular-audio-wave.js
@@ -15107,7 +15107,7 @@ class CircularAudioWave {
         "sunburst" !== this.opts.mode && (this.chartOption.series[0].animation = !1, this.chartOption.series[2].rippleEffect.period = 150 / this.bpm)
     }
     play() {
-      if (this.sourceNode && this.sourceNode.buffer && this.playing === false) {
+      if (this.sourceNode && this.sourceNode.buffer && this.context.state !== 'suspended' && this.playing === false) {
         this.playing = true;
         this.presetOption();
         this.sourceNode.start(0);
