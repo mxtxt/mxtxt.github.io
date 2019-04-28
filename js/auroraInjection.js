023ad3bb4b77b3ba8764759a02f14e28d1b4014b
SeverinJB <32867365+SeverinJB@users.noreply.github.com>
2019-04-28 15:45:37 +0200

    .

--- js/auroraInjection.js
+++ js/auroraInjection.js
@@ -4,8 +4,6 @@ function injectAurora(){
 
   if (!injected) {
     frameContent.addEventListener("load", function() {
-      bodyArea = frameContent.contentWindow.document.getElementsByTagName("body")[0];
-      bodyArea.insertAdjacentHTML('afterbegin','<div id="auroraWrapper" style="display: block;"><button onclick="speak()">Pause/Resume</button><div id="chart-container" onclick="wave.play()"></div></div>');
 
       var jquery   = frameContent.contentWindow.document.createElement("script");
       jquery.type  = "text/javascript";
